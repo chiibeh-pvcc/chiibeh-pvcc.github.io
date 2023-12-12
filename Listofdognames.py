7828f801880482b4ffb3fd16075ba26af66c3cd0
chiibeh-pvcc <150191779+chiibeh-pvcc@users.noreply.github.com>
2023-12-12 18:28:01 -0500

    Update Listofdognames.py

--- Listofdognames.py
+++ Listofdognames.py
@@ -54,8 +54,6 @@ def main():
         dogs[i] = dogs[i] + " Kingsley-Ibeh" #Change Stevens to YOUR last name added my Cat's name Beef Wellington
     print(dogs)
 
-input('Press ENTER to exit')
-
 main()
 
 
